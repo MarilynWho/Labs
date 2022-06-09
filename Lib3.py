0a2dd7b06897460ca1a3e064ce8eb7a0009f9ff6
Vi <vi@localhost.localdomain>
2022-06-09 14:10:25 +0100

    clen up

--- Lib3.py
+++ Lib3.py
@@ -3,7 +3,6 @@ import datetime
 
 def file_date(filename):
   # Create the file in the current directory
-  file = open(filename, "w").close()
   timestamp = os.path.getmtime(filename)
   # Convert the timestamp into a readable format, then into a string
   date = datetime.datetime.fromtimestamp(timestamp)
@@ -12,5 +11,6 @@ def file_date(filename):
   # Hint: how many characters are in “yyyy-mm-dd”? 
   return ("{}".format(date_time)[0:10])
 
-print(file_date("newfile.txt")) 
+open("newfile.txt", "w").close()
+print(file_date("newfile.txt"))
 # Should be today's date in the format of yyyy-mm-dd
